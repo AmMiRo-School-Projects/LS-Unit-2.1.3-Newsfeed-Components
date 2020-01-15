31f4e7a345938cc98f8ae79f2e138088ccd56765
Amos Rose <amosroseacct@gmail.com>
2020-01-15 14:56:23 -0700

    MVP reached

--- Article/Article.js
+++ Article/Article.js
@@ -3,7 +3,7 @@
 const data = [
   {
     title: 'Lambda School Students: "We\'re the best!"',
-    date: 'Nov 5th, 2018',
+    date: "Nov 5th, 2018",
     firstParagraph: `Lucas ipsum dolor sit amet ben twi'lek padmé darth darth darth moff hutt organa twi'lek. Ben amidala secura skywalker lando
         moff wicket tatooine luke.Solo wampa wampa calrissian yoda moff.Darth grievous darth gonk darth hutt.Darth baba skywalker
         watto fett jango maul han.Mon ewok sidious sidious lando kenobi grievous gamorrean solo.Yoda wedge utapau darth calamari.
@@ -23,8 +23,8 @@ const data = [
         moff calamari mon obi-wan. Solo grievous lando coruscant. Jinn darth palpatine obi-wan mon.`
   },
   {
-    title: 'Javascript and You, ES6',
-    date: 'May 7th, 2019',
+    title: "Javascript and You, ES6",
+    date: "May 7th, 2019",
     firstParagraph: `Alohamora wand elf parchment, Wingardium Leviosa hippogriff, house dementors betrayal. Holly, Snape centaur portkey ghost
         Hermione spell bezoar Scabbers. Peruvian-Night-Powder werewolf, Dobby pear-tickle half-moon-glasses, Knight-Bus. Padfoot
         snargaluff seeker: Hagrid broomstick mischief managed. Snitch Fluffy rock-cake, 9 ¾ dress robes I must not tell lies. Mudbloods
@@ -43,8 +43,8 @@ const data = [
         sing above the ground, Ginny Weasley bright red. Fanged frisbees, phoenix tears good clean match.`
   },
   {
-    title: 'React vs Angular vs Vue',
-    date: 'June 7th, 2019',
+    title: "React vs Angular vs Vue",
+    date: "June 7th, 2019",
     firstParagraph: `Bulbasaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ivysaur Lorem ipsum dolor sit amet, consectetur adipiscing
         elit. Venusaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charmander Lorem ipsum dolor sit amet, consectetur
         adipiscing elit. Charmeleon Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charizard Lorem ipsum dolor sit amet,
@@ -71,8 +71,8 @@ const data = [
         Castform Lotad the power that's inside Burnt Berry Makuhita. Ghost Ariados Corphish Dusclops Golbat Gligar Zweilous.`
   },
   {
-    title: 'Professional Software Development in 2019',
-    date: 'Jan 1st, 2019',
+    title: "Professional Software Development in 2019",
+    date: "Jan 1st, 2019",
     firstParagraph: `Hodor hodor HODOR! Hodor hodor - hodor, hodor. Hodor hodor... Hodor hodor hodor; hodor hodor. Hodor hodor hodor, hodor, hodor
           hodor. Hodor, hodor. Hodor. Hodor, hodor - hodor... Hodor hodor hodor; hodor HODOR hodor, hodor hodor?! Hodor hodor, hodor.
           Hodor hodor hodor hodor hodor! Hodor hodor - HODOR hodor, hodor hodor hodor hodor hodor; hodor hodor? `,
@@ -85,6 +85,26 @@ const data = [
     thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
           Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
           Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
+  },
+  {
+    title: "Student's Favorite Pets",
+    date: "Jan 14th, 2020",
+    firstParagraph:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo eveniet aliquid neque atque, voluptate debitis odio enim non voluptatem quae pariatur, minima fuga obcaecati veritatis nemo modi excepturi? Veniam, quisquam.",
+    secondParagraph:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo eveniet aliquid neque atque, voluptate debitis odio enim non voluptatem quae pariatur, minima fuga obcaecati veritatis nemo modi excepturi? Veniam, quisquam.",
+    thirdParagraph:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo eveniet aliquid neque atque, voluptate debitis odio enim non voluptatem quae pariatur, minima fuga obcaecati veritatis nemo modi excepturi? Veniam, quisquam."
+  },
+  {
+    title: "What the Apocalypse Means for Your Career",
+    date: "Jan 1st, 2012",
+    firstParagraph:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo eveniet aliquid neque atque, voluptate debitis odio enim non voluptatem quae pariatur, minima fuga obcaecati veritatis nemo modi excepturi? Veniam, quisquam.",
+    secondParagraph:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo eveniet aliquid neque atque, voluptate debitis odio enim non voluptatem quae pariatur, minima fuga obcaecati veritatis nemo modi excepturi? Veniam, quisquam.",
+    thirdParagraph:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo eveniet aliquid neque atque, voluptate debitis odio enim non voluptatem quae pariatur, minima fuga obcaecati veritatis nemo modi excepturi? Veniam, quisquam."
   }
 ];
 
@@ -112,3 +132,45 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+function createArticle(obj) {
+  // creates elements with classes
+  const article = document.createElement("div");
+  article.classList.add("article");
+  const articleTitle = document.createElement("h2");
+  const articleDate = document.createElement("p");
+  articleDate.classList.add("date");
+  const articleParagraphOne = document.createElement("p");
+  const articleParagraphTwo = document.createElement("p");
+  const articleParagraphThree = document.createElement("p");
+  const articleButton = document.createElement("span");
+  articleButton.classList.add("expandButton");
+  // appends elements to parrent
+  article.append(articleTitle);
+  article.append(articleDate);
+  article.append(articleParagraphOne);
+  article.append(articleParagraphTwo);
+  article.append(articleParagraphThree);
+  article.append(articleButton);
+  // adds text content
+  articleTitle.textContent = obj.title;
+  articleDate.textContent = obj.date;
+  articleParagraphOne.textContent = obj.firstParagraph;
+  articleParagraphTwo.textContent = obj.secondParagraph;
+  articleParagraphThree.textContent = obj.thirdParagraph;
+  articleButton.textContent = "Click to Expand";
+  // togles button
+  articleButton.addEventListener("click", () => {
+    article.classList.toggle("article-open");
+  });
+  // returns article
+  return article;
+}
+
+// calls parent articles div
+const articles = document.querySelector(".articles");
+
+// appends articles with createArticle applied to each obj in data
+data.forEach(item => {
+  articles.append(createArticle(item));
+  // console.log(item);
+});
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,12 +1,12 @@
 /* This is the data we will be using, study it but don't change anything, yet. */
 
 let menuItems = [
-  'Students',
-  'Faculty',
+  "Students",
+  "Faculty",
   "What's New",
-  'Tech Trends',
-  'Music',
-  'Log Out'
+  "Tech Trends",
+  "Music",
+  "Log Out"
 ];
 
 /* 
@@ -33,3 +33,28 @@ let menuItems = [
   Step 6: add the menu component to the DOM.
   
 */
+
+function menuCreator(arr) {
+  // creates menu element
+  const menu = document.createElement("div");
+  menu.classList.add("menu");
+  const list = document.createElement("ul");
+
+  menu.append(list);
+
+  arr.forEach(obj => {
+    const li = document.createElement("li");
+    li.textContent = obj;
+    list.append(li);
+  });
+  const menuButton = document.querySelector(".menu-button");
+
+  menuButton.addEventListener("click", () => {
+    menu.classList.toggle("menu--open");
+  });
+  return menu;
+}
+
+const header = document.querySelector(".header");
+
+header.prepend(menuCreator(menuItems));
